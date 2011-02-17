e382ce29e19c642052022c84bc72595b35f0290a
ping.gong <ping.gong@amlogic.com>
2011-02-17 15:47:42 +0800

    link libbinder lib

--- jni/Android.mk
+++ jni/Android.mk
@@ -17,7 +17,7 @@ LOCAL_C_INCLUDES := $(LIBPLAYER_PATH)/amplayer/player/include \
     $(JNI_H_INCLUDE) 
 
 LOCAL_STATIC_LIBRARIES := libamplayer libamcodec libavformat libavcodec libavutil libamadec 
-LOCAL_SHARED_LIBRARIES += libutils libmedia libz
+LOCAL_SHARED_LIBRARIES += libutils libmedia libz libbinder
 
 LOCAL_PRELINK_MODULE := false
 
