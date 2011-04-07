e260775d9a66c2225dd121f42eca62d2bc3c91a9
bin.jiang <bin.jiang@amlogic.com>
2011-04-07 17:40:00 +0800

    get videoinfo when has video

--- jni/com_farcore_playerservice_AmPlayer.c
+++ jni/com_farcore_playerservice_AmPlayer.c
@@ -199,8 +199,10 @@ jobject MediaInfoContext_create(JNIEnv *env,media_info_t *msgt){
 
     (*env)->SetLongField(env,meta_obj, (*env)->GetFieldID(env, meta_cls, "filesize", "J"), msgt->stream_info.file_size); 
     (*env)->SetIntField(env,meta_obj, (*env)->GetFieldID(env, meta_cls, "duration", "I"), msgt->stream_info.duration); 
-	(*env)->SetIntField(env,meta_obj, (*env)->GetFieldID(env, meta_cls, "width", "I"), msgt->video_info[0]->width); 
-	(*env)->SetIntField(env,meta_obj, (*env)->GetFieldID(env, meta_cls, "height", "I"), msgt->video_info[0]->height); 
+	if (msgt->stream_info.total_video_num >0) { 
+		(*env)->SetIntField(env,meta_obj, (*env)->GetFieldID(env, meta_cls, "width", "I"), msgt->video_info[0]->width); 
+		(*env)->SetIntField(env,meta_obj, (*env)->GetFieldID(env, meta_cls, "height", "I"), msgt->video_info[0]->height); 
+	}
 #if 0
     
     if(msgt->stream_info.total_video_num >0){      
