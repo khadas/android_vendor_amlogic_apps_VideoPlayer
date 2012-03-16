f79d89a630092754b6f943b7b26405bff0317a75
zhi.zhou <zhi.zhou@amlogic.com>
2012-03-16 20:48:12 +0800

    add libamavutils, all other used LibPlayer need it ...

--- jni/Android.mk
+++ jni/Android.mk
@@ -17,8 +17,8 @@ LOCAL_C_INCLUDES := $(LIBPLAYER_PATH)/amplayer/player/include \
     $(LIBPLAYER_PATH)/amffmpeg \
     $(JNI_H_INCLUDE) 
 
-LOCAL_STATIC_LIBRARIES := libamplayer libamcodec libavformat libavcodec libavutil libamadec 
-LOCAL_SHARED_LIBRARIES += libutils libmedia libz libbinder libdl libcutils libc
+LOCAL_STATIC_LIBRARIES := libamplayer libamcodec libavformat libavcodec libavutil libamadec  
+LOCAL_SHARED_LIBRARIES += libutils libmedia libz libbinder libdl libcutils libc libamavutils
 
 LOCAL_PROGUARD_ENABLED := disabled
 
